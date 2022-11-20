bc6a3cfddfb47e45232512c575b34776b94285d9
Skyes Crawford <akashiseijurou414@gmail.com>
2022-11-20 21:10:40 +0700

    initialize project and create models

---
+++ Models/Country.cs
@@ -0,0 +1,11 @@
+namespace PokemonReviewApp.Models
+{
+    public class Country
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public ICollection<Owner> Owners { get; set; }
+        
+        
+    }
+}
